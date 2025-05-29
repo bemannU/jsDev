5ddc273ea013f17bc76284d2c3ab378a320f837f
bemannU <zhawkins2468@gmail.com>
2025-05-29 10:40:12 +0930

    .

--- tutorials/JavaBook/index.js
+++ tutorials/JavaBook/index.js
@@ -134,33 +134,36 @@
 //    console.log("you're my type");
 //}
 
-const myCheckBox = document.getElementById("myCheckBox");
-const visaBtn = document.getElementById("visaBtn");
-const masterCBtn = document.getElementById("masterCBtn");
-const payPalBtn = document.getElementById("payPalBtn");
-const mySubmit = document.getElementById("mySubmit");
-const subResult = document.getElementById("subResult");
-const paymentResult = document.getElementById("paymentResult");
+//checked feature:
 
-mySubmit.onclick = function(){
+//const myCheckBox = document.getElementById("myCheckBox");
+//const visaBtn = document.getElementById("visaBtn");
+//const masterCBtn = document.getElementById("masterCBtn");
+//const payPalBtn = document.getElementById("payPalBtn");
+//const mySubmit = document.getElementById("mySubmit");
+//const subResult = document.getElementById("subResult");
+//const paymentResult = document.getElementById("paymentResult");
 
-    if(myCheckBox.checked){
-        subResult.textContent = "you are subscribed!";
-    }
-    else{
-        subResult.textContent = "you are not subscribed:(";
-    }
+//mySubmit.onclick = function(){
+//
+//    if(myCheckBox.checked){
+//        subResult.textContent = "you are subscribed!";
+//    }
+//    else{
+//        subResult.textContent = "you are not subscribed:(";
+//    }
+//
+//    if(visaBtn.checked){
+//        paymentResult.textContent = "you are paying with Visa";
+//    }
+//     else if(masterCBtn.checked){
+//         paymentResult.textContent = "you are paying with MasterCard";
+//     }
+//     else if(payPalBtn.checked){
+//         paymentResult.textContent = "you are paying with PayPal";
+//     }
+//     else{
+//         paymentResult.textContent = "select payment type";
+//     }
+// }
 
-    if(visaBtn.checked){
-        paymentResult.textContent = "you are paying with Visa";
-    }
-    else if(masterCBtn.checked){
-        paymentResult.textContent = "you are paying with MasterCard";
-    }
-    else if(payPalBtn.checked){
-        paymentResult.textContent = "you are paying with PayPal";
-    }
-    else{
-        paymentResult.textContent = "select payment type";
-    }
-}
