9024de0495999a76b0b28a06fc8c8fd0aaae6780
bemannU <zhawkins2468@gmail.com>
2025-05-29 10:35:50 +0930

    .

--- tutorials/JavaBook/index.js
+++ tutorials/JavaBook/index.js
@@ -134,3 +134,33 @@
 //    console.log("you're my type");
 //}
 
+const myCheckBox = document.getElementById("myCheckBox");
+const visaBtn = document.getElementById("visaBtn");
+const masterCBtn = document.getElementById("masterCBtn");
+const payPalBtn = document.getElementById("payPalBtn");
+const mySubmit = document.getElementById("mySubmit");
+const subResult = document.getElementById("subResult");
+const paymentResult = document.getElementById("paymentResult");
+
+mySubmit.onclick = function(){
+
+    if(myCheckBox.checked){
+        subResult.textContent = "you are subscribed!";
+    }
+    else{
+        subResult.textContent = "you are not subscribed:(";
+    }
+
+    if(visaBtn.checked){
+        paymentResult.textContent = "you are paying with Visa";
+    }
+    else if(masterCBtn.checked){
+        paymentResult.textContent = "you are paying with MasterCard";
+    }
+    else if(payPalBtn.checked){
+        paymentResult.textContent = "you are paying with PayPal";
+    }
+    else{
+        paymentResult.textContent = "select payment type";
+    }
+}
